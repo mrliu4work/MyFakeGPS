9267f13247c9eccda618cf026ef02fda4d6c53c9
mrliu4work <mrliu4work@gmail.com>
2026-07-30 04:08:11 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,4 +28,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    // 地圖套件 OpenStreetMap
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
 }
